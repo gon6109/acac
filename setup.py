a77889d321e038ccd357f9ca1c7a2e275394c83c
GON <hutabasuzukiryu@live.jp>
2019-12-17 12:12:08 +0900

    update: setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ import sys
 import argparse
 import subprocess
 import shutil
+import glob
+
+def FormatContent(content):
+    res = content.replace('/images/acac2019', 'images/acac2019')
+    return res
 
 parser = argparse.ArgumentParser(description='Set up ACAC icml')
-parser.add_argument('output', metavar='OUTPUT', type=str, nargs=1, help='output path')
+parser.add_argument('output', metavar='OUTPUT', type=str, help='output path')
 parser.add_argument('--md', help='input markdown directory path')
 parser.add_argument('--image', help='input image directory path')
 
@@ -31,7 +36,21 @@ image = args.image
 if os.path.exists(os.path.dirname(__file__) + "/" + tmp):
     shutil.rmtree(os.path.dirname(__file__) + "/" + tmp)
 
-shutil.copytree(md, os.path.dirname(__file__) + "/" + tmp + "/md")
+shutil.copytree(md, os.path.dirname(__file__) + "/" + tmp)
 shutil.copytree(image, os.path.dirname(__file__) + "/" + tmp + "/images")
 
+cwd = os.getcwd()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+formated = ""
+for path in glob.glob(tmp + "/*.md"):
+    with open(path, encoding="utf8") as f:
+        content = f.read()
+    formated += FormatContent(content)
+
+with open(tmp + "/formated.md", mode='w', encoding="utf8") as f:
+    f.write(formated)
+
+os.chdir(tmp)
+pandoc = ["pandoc", "formated.md", "-s", "-o", cwd + "/" + output]
+subprocess.call(pandoc)
