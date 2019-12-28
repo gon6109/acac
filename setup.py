26327f06427f097bf762a4f56828b03ceb747842
GON <hutabasuzukiryu@live.jp>
2019-12-28 13:31:21 +0900

    add: FormatQuote

--- setup.py
+++ setup.py
@@ -6,11 +6,17 @@ import shutil
 import glob
 import re
 
-def FormatContent(content):
+def FormatQuote(content, prefix):
+    res = re.sub(r"\[\^(.+?)\]", r"[^" + str(prefix) + r"-\1]", content, flags=re.MULTILINE)
+    res = re.sub(r"^(\[\^.+?\]:.*)$", "\n" + r"\1" + "\n", res, flags=re.MULTILINE)
+    return res
+
+def FormatContent(content, prefix):
     res = content.replace('/images/acac2019', 'images/acac2019')
     res = re.sub(r"([^!])\[(.*)\]\((.*)\)", r"\1\2(\3)", res, flags=re.MULTILINE)
     res = re.sub(r"^#", r"##", res, flags=re.MULTILINE)
     res = re.sub(r"  " + "\n" + r"(?!\[\^\d+\])", r"", res, flags=re.MULTILINE|re.DOTALL)
+    res = FormatQuote(res, prefix)
     res = re.sub(r"^\+\+\+.*title\s*=\s*\"(.*?)\".*authors\s*=\s\[\"(.*?)\"\].*\+\+\+",
      r"# \1" + "\n" + r"###### \2", res, flags=re.DOTALL)
     res += "\n"
@@ -51,12 +57,14 @@ cwd = os.getcwd()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 formated = ""
+i = 0
 for path in glob.glob(tmp + "/*.md"):
     if path.count("_index") > 0:
         continue
     with open(path, encoding="utf8") as f:
         content = f.read()
-    formated += FormatContent(content)
+    formated += FormatContent(content, i)
+    i += 1
 
 with open(tmp + "/formated.md", mode='w', encoding="utf8") as f:
     f.write(formated)
